f4cb6b693c1b815e1cfc80deb1ef4213f6d4b264
Nishant Sharma <149394482+nishant-s-anlst@users.noreply.github.com>
2025-09-20 07:08:53 +0530

    Update and rename Subqueries-vs-CTE.sql to Subqueries_vs_CTE.sql

---
+++ Concepts/Subqueries_vs_CTE.sql
@@ -0,0 +1,38 @@
+-- Find companies that have at least 2 users who speak both English and German.
+
+-- Sample table
+create table company_users 
+(
+company_id int,
+user_id int,
+language varchar(20)
+);
+
+insert into company_users values (1,1,'English')
+,(1,1,'German')
+,(1,2,'English')
+,(1,3,'German')
+,(1,3,'English')
+,(1,4,'English')
+,(2,5,'English')
+,(2,5,'German')
+,(2,5,'Spanish')
+,(2,6,'German')
+,(2,6,'Spanish')
+,(2,7,'English');
+
+--Solution
+SELECT company_id 
+FROM company_users
+WHERE user_id IN (
+    SELECT user_id 
+    FROM company_users
+    WHERE language IN ('English','German')
+    GROUP BY user_id
+    HAVING COUNT(DISTINCT language) = 2
+)
+GROUP BY company_id
+HAVING COUNT(DISTINCT user_id) >= 2;
+
+
+
