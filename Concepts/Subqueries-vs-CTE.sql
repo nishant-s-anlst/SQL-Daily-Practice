3f3e652ec3ac50e72b07799fb60ee93bb287fde2
Nishant Sharma <149394482+nishant-s-anlst@users.noreply.github.com>
2025-09-20 07:02:25 +0530

    Update Subqueries-vs-CTE.sql

--- Concepts/Subqueries-vs-CTE.sql
+++ Concepts/Subqueries-vs-CTE.sql
@@ -1,6 +1,5 @@
 ### Find companies that have at least 2 users who speak both English and German.
 
-```
 select company_id from company_users
 where user_id in (
 select user_id from company_users
@@ -9,5 +8,5 @@ group by user_id
 having COUNT(distinct language)=2)
 group by company_id
 having COUNT(distinct user_id)>=2
-```
+
 
